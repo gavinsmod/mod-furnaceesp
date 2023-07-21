de30463c96b2895a24385dee5bd5adb67d5c655f
Gavin <gavinpease@gmail.com>
2023-07-21 12:24:55 -0600

    Remove logging, and optionally removed mixins file if required

--- src/main/java/com/peasenet/mod/FurnaceEspMod.java
+++ src/main/java/com/peasenet/mod/FurnaceEspMod.java
@@ -6,14 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FurnaceEspMod implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
         GavinsMod.addMod(new ModFurnaceEsp());
     }
 }
